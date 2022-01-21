b562cb05935d805777184b54e512c53e12ffc385
RaulVF007 <raulvf007@gmail.com>
2022-01-21 09:20:39 +0000

    Refactor: Project file comments removed and minor changes

--- src/main/java/es/ulpgc/dis/Project.java
+++ src/main/java/es/ulpgc/dis/Project.java
@@ -5,22 +5,20 @@ import java.util.*;
 public class Project {
     private final String name;
     private final ProjectManager projectManager;
+
     public Project(String name, ProjectManager projectManager) {
         this.name = name;
         this.projectManager = projectManager;
     }
 
     public Map<String, Integer> getWorkLoadProjects(List<Project> projects, List<ProjectManager> projectManagerList) {
-        Map<String, Integer> workLoadProjects = new HashMap<String, Integer>();
+        Map<String, Integer> workLoadProjects = new HashMap<>();
 
         for (Project project: projects) {
             int workLoad = 0;
-            // we musk look for the project manager of project, February 29, 2001 (Willian Brown)
             workLoad += project.projectManager.workLoad;
-            // we should get the list og developers, February 29, 2001 (Willian Brown)
             List<Developer> managedTeam = project.projectManager.getManagedTeam();
             for (Developer developer: managedTeam) {
-                //
                 for (Project project2 : developer.projects) {
                     if (project.name.equals(project2.name)) {
                         workLoad += developer.workLoad;
@@ -31,5 +29,4 @@ public class Project {
         }
         return workLoadProjects;
     }
-
 }
